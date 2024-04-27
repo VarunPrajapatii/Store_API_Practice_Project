9f43ab84ef22554631cd034edcb158d98255bde5
Varun Prajapati <varunprajapati123@gmail.com>
2024-04-28 01:51:53 +0530

    added sort fields ets... query strings in controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,52 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error("testing async errors");
-    res.status(200).json({msg: 'product testing route'});
+    const products = await Product.find({price : {$gt: 30}}).sort("price");
+    res.status(200).json({products, nbHits: products.length});
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'product route'});
+    const {featured, company, name, sort, fields, numericFilters} = req.query;
+    const queryObject = {}
+    
+    if(featured) {
+        queryObject.featured = featured === 'true'? true : false;
+    }
+    if(company) {
+        queryObject.company = company;
+    }
+    if(name) {
+        queryObject.name = { $regex: name, $options: 'i'};
+    }
+    //numericFilters
+
+
+
+
+
+
+
+    let result = Product.find(queryObject);
+    //sort
+    if(sort) {
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList);
+    } else {
+        result = result.sort("createAt");
+    }
+    //fields
+    if(fields) {
+        const fieldsList = fields.split(",").join(" ");
+        result = result.select(fieldsList);
+    }
+    
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
+
+    const products = await result;
+    res.status(200).json({products, nbHits: products.length});
 }
 
 module.exports = {
