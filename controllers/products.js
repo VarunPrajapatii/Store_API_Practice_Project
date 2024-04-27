64ed3379c96d177d4c8fd3a24d910f0f75abecac
Varun Prajapati <varunprajapati123@gmail.com>
2024-04-28 02:49:50 +0530

    done with numericFilters too

--- controllers/products.js
+++ controllers/products.js
@@ -19,7 +19,24 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i'};
     }
     //numericFilters
-
+    if(numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|>=|=|<=)\b/g;
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split("-");
+            if(options.includes(field)) {
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        });
+    }
 
 
 
